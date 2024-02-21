76e3b9dc43d15abc14c21dad1364c624b73ed809
Özkan Parlakkılıç <97343873+Ekoizm@users.noreply.github.com>
2024-02-21 14:04:50 +0300

    Taksimetre class eklendi.

--- src/Week_01/Taksimetre.java
+++ src/Week_01/Taksimetre.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Taksimetre {
     public static void main(String[] args) {
         int km;
-        double perKm = 2.20 , total , startPrice = 1 ;
+        double perKm = 2.20 , total , startPrice = 10 ;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Mesafei KM cinsinden giriniz :");
