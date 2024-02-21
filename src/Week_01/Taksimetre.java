47ab6db9ec83856ed7591a4db4da687080eab28f
Özkan Parlakkılıç <97343873+Ekoizm@users.noreply.github.com>
2024-02-21 14:04:03 +0300

    NotOrtalamasi class eklendi.

---
+++ src/Week_01/Taksimetre.java
@@ -0,0 +1,18 @@
+package Week_01;
+import java.util.Scanner;
+public class Taksimetre {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20 , total , startPrice = 10 ;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafei KM cinsinden giriniz :");
+        km = input.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam tutar :" + total);
+    }
+}
