ae386f986c9c679e5ee180a377faf3126c29f308
Özkan Parlakkılıç <97343873+Ekoizm@users.noreply.github.com>
2024-02-21 13:09:53 +0300

    HipotenusBulma class eklendi.

---
+++ src/Week_01/AritmetikIslemOnceligi.java
@@ -0,0 +1,25 @@
+package Week_01;
+import java.util.Scanner;
+public class AritmetikIslemOnceligi {
+
+    public static void main(String[] args) {
+
+        int sayi1,sayi2,sayi3;
+        double islem;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1. sayıyı giriniz : ");
+        sayi1 = input.nextInt();
+
+        System.out.print("2. sayıyı giriniz : ");
+        sayi2 = input.nextInt();
+
+        System.out.print("3. sayıyı giriniz : ");
+        sayi3 = input.nextInt();
+
+        islem = sayi1 + sayi2 * sayi3 - sayi2;
+
+        System.out.println("Sonuç : " + islem);
+
+    }
+}
