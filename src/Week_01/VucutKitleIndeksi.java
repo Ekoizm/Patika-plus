f4f467128c2306741ef98de5238dce8098773d86
Özkan Parlakkılıç <97343873+Ekoizm@users.noreply.github.com>
2024-02-21 14:30:36 +0300

    VucutKitleIndeksi class'ına açıklama satırı eklendi.

--- src/Week_01/VucutKitleIndeksi.java
+++ src/Week_01/VucutKitleIndeksi.java
@@ -2,17 +2,20 @@ package Week_01;
 import java.util.Scanner;
 public class VucutKitleIndeksi {
     public static void main(String[] args) {
-        double boy,kilo,indeks;
+        double boy,kilo,indeks; //Değişkenleri tanımladık
         Scanner input = new Scanner(System.in);
 
+        //Kullanıcıdan veri istedik
         System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
         boy = input.nextDouble();
 
         System.out.print("Lütfen kilonuzu giriniz : ");
-        kilo = input.nextInt();
+        kilo = input.nextDouble();
 
+        //Girilen bilgiler ile formülümüzü uyguladık
         indeks = (kilo / (boy*boy));
 
+        //Çıkan sonucu ekrana yazdırdık
         System.out.println("Vücut kütle indeksiniz " + indeks);
 
     }
