fafed52e3fa9eb0248d77308f43c851795e3ffe2
Özkan Parlakkılıç <97343873+Ekoizm@users.noreply.github.com>
2024-03-27 10:25:56 +0300

    Book main classı'nda güncelleme yapıldı

--- src/Week_05/Book/Main.java
+++ src/Week_05/Book/Main.java
@@ -7,11 +7,11 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Java Programming", 400);
-        Book book2 = new Book("Python Programming", 350);
-        Book book3 = new Book("C++ Programming", 500);
-        Book book4 = new Book("Data Structures", 300);
-        Book book5 = new Book("Algorithms", 450);
+        Book book1 = new Book("Sefiller", 400);
+        Book book2 = new Book("Beyaz Gemi", 350);
+        Book book3 = new Book("Simyacı", 500);
+        Book book4 = new Book("Şeker Portakalı", 300);
+        Book book5 = new Book("Büyük Hizmetkar", 450);
 
         Set<Book> bookSet = new HashSet<>();
         bookSet.add(book1);
